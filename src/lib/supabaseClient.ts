66185322c8eac42cf5e1747be3aea82172854320
Viet Hung Pham <hung.v.pham002@gmail.com>
2025-06-26 22:28:05 -0400

    v1.0.6: More optimization

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,21 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+let supabaseInstance: SupabaseClient | null = null;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const getSupabase = () => {
+    if (supabaseInstance) {
+        return supabaseInstance;
+    }
+
+    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+    const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+    if (!supabaseUrl || !supabaseKey) {
+        throw new Error("Supabase URL and Key must be defined in environment variables.");
+    }
+
+    supabaseInstance = createClient(supabaseUrl, supabaseKey);
+    return supabaseInstance;
+};
+
+export default getSupabase;
